f0e68e446e8f30a7ccc5a6de4c70f350e94bf228
javadevelopr <java.developer.cl@gmail.com>
2020-02-27 07:25:06 -0800

    parser wip

--- GourdianTest/parse.py
+++ GourdianTest/parse.py
@@ -1,112 +1,74 @@
 #!/usr/bin/env python
-# File Name: partition.py
+# File Name: parseJson.py
 #
-# Date Created: Feb 04,2020
+# Date Created: Feb 26,2020
 #
-# Last Modified: Mon Feb 17 14:47:57 2020
+# Last Modified: Thu Feb 27 07:24:15 2020
 #
 # Author: samolof
 #
 # Description:	
 #
 ##################################################################
-import boto3
 import json
-from pyspark.sql import SparkSession, functions as sqlFunctions
-from pyspark.sql import dataframe
-from gourdian import gytpes
+import argparse
+import sys
+import logger
+from pyspark.sql import SparkSession
+
+parser = argparse.ArgumentParser()
+parser.add_argument('jsonFile', metavar='CONFIG_FILE',nargs='?', type=str, help='The Json Configuration File')
+parser.add_argument('sourcePath')
+
+# partition_function = getattr(functions, 'longlatpartitioner')
+
+SOURCE_PATHS = {
+        'epa_aqs' : {
+            'ozone_daily_summary' : "s3a://insight-gourdian-epaaqs-ozone-m/",
+            'so2_daily_summary' : "s3a://insight-gourdian-epaaqs-so2/",
+            'co_daily_summary' : "s3a://insight-gourdian-epaaqs-co/",
+            'no2_daily_summary': "s3a://insight-gourdian-epaaqs-no2/"
+            },
+        'noaa': {
+            "global_summary_of_day" : "s3a://insight-gourdian-noaa-global-summary-of-day-joined/"
+        },
+        'usgs_comcat' : {
+            "summary": "s3a://insight-gourdian-sources/usgs_comcat/"
+        }
+}
 
 
+if __name__=="__main__":
+    spark = SparkSession.builder.appName('Gourdnet_Versioner').getOrCreate()
+    args = parser.args()
+    jsFile = args.jsonFile
 
-def parseLayout(layout: dict, columns: list, gtype_columns: list):
-    layout_name = layout['layout_name']
-    layout_handler = LayoutHandlers[layout_name] 
+    chunkers = []; tables = {}
+    js = None
+    with open(jsFile, 'r') as j:
+        js = json.loads(j.read())
 
 
-def parseSource(sourceName: str, source: dict ):
-        columns = source['columns'] 
-        gtype_columns = source['gtype_columns']
+    dataset = json["dataset"]["name"]
+    sources =  json["dataset"]["sources"]
+
+    for source in sources:
+        #table - source:[table1:key,keyFunction, table2]
+        name = source['name']
+        columns = source['column']
+        try:
+            transformColumns = source['transformColumns']
+            transformFunctions = source['transformFunnction']
+    
+        except KeyError as e:
+            transformColumns = transformFunctions = None
+
         layouts = source['layouts']
 
         for layout in layouts:
-            parseLayout(layout, columns, gtype_columns)
-  
-def getSourceFromRef(ref: str, sources: list) -> dict:
-    """ A quirky way to handle repeated fields in json to keep the json file small. 
-    For a key that repeats fields and values from another key we reference that key and
-    parse it instead for the correct values
-    """
-    source = list(filter(lambda s: s['name'] == ref, sources))
+            layoutName, keys, keyFunctions = parseLayout(layout)
 
-    return len(source) > 0 and source[0] or None
-
-
-def __s3getBucketSize(bucket):
-    pass
-
-def __getPartitionHash():
-    pass
-
-
-
-def noaa_transform(df: dataframe.Dataframe):
-    pass
-
-
-
-def epa_aqs_transform(df: dataframe.Dataframe ):
-
-    #rename columns
-    df = df.withColumnRenamed('Date Local','Date')
-
-
-    #split columns
-
-
-    #drop columns
-
-
-    #change Type?
-
-
-
-#To do transform column types to Gtypes
-#df = spark.read.load("s3a://insight-gourdian-epaaqs-co/*.csv", format="csv", sep=",", inferSchema="true", header="true")
-            
-
-
-def setParameters(jsonFile: str):
-    global datasetName 
-    ref = None
-    with open(jsonFile) as jfile:
-        js = json.loads(jfile.read())
-        datasetName = js['dataset']['name']
-        sources = js['dataset']['sources']
-        for source in sources:
-            sourceName = source['name']
-            ref = source['ref']
-            if ref and len(ref) > 0: 
-                source = getSourceFromRef(ref, sources)
-            parseSource(sourceName, source)
-            
-
-        
-def main():
-   spark = SparkSession.builder.appName('Versioner').getOrCreate()
-
-
-    
 
 
 
     spark.stop()
-
-#load json
-#
-
-#spark=SparkSession.builder.appName('Versioner').getOrCreate()
-#data=spark.read.load("../Data/chunk_noaa.csv",format="csv", sep=",", inferSchema="true", header="true" )
-
-#print(data.columns)
-
-
